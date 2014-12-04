aac32e7da753f72ae9484c07ef26d815df4d898f
Uemmra3 <Uemmra3@gmail.com>
2014-12-04 21:39:40 +0900

    initial commit for angularJS trial

---
+++ myscript.js
@@ -0,0 +1,31 @@
+// var mainCtrl = function($scope){
+// 	$scope.users = [
+// 		{"name": "Uemura", "score":46.89},
+// 		{"name": "tanaka", "score":38.22},
+// 		{"name": "yamada", "score":11.11},
+// 		{"name": "yamada", "score":46.78},
+// 		{"name": "endou", "score":87.89},
+// 		{"name": "uno", "score":23.22},
+// 		{"name": "yasuda", "score":5.11},
+// 		{"name": "ikehata", "score":94.78},
+// 	]
+
+// 	$scope.today = new Date();
+
+
+// }
+
+// var userItemCtrl = function ($scope) {
+// 	// body...
+// 	$scope.increment = function () {
+// 		// body...
+// 		$scope.user.score++;
+// 	}
+// }
+
+
+var mainCtrl = function($scope){
+	$scope.addUser = function(){
+		
+	}
+}
